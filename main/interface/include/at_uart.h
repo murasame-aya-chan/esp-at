bfa109e65d1d2448fcfb3c056f223c1edaa6837e
murasame-aya-chan <65704016+murasame-aya-chan@users.noreply.github.com>
2024-07-31 18:34:30 +0900

    FW-HY

--- main/interface/include/at_uart.h
+++ main/interface/include/at_uart.h
@@ -41,7 +41,7 @@
 #else
 #define AT_UART_RX_BUFFER_SIZE                      2048                    /**< uart rx buffer size */
 #define AT_UART_TX_BUFFER_SIZE                      4096                    /**< uart tx buffer size */
-#define AT_UART_QUEUE_SIZE                          3000                    /**< uart queue size */
+#define AT_UART_QUEUE_SIZE                          30                      /**< uart queue size */
 #endif
 
 /**
