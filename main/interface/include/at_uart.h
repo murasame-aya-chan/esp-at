1c7b0ce357a135166304f524fb29f3c7a8ee5d71
murasame-aya-chan <65704016+murasame-aya-chan@users.noreply.github.com>
2024-07-31 18:07:42 +0900

    HY-ESP32C6

--- main/interface/include/at_uart.h
+++ main/interface/include/at_uart.h
@@ -26,7 +26,7 @@
  *                Macros
  *******************************************************/
 #define AT_UART_BAUD_RATE_MAX                       SOC_UART_BITRATE_MAX    /**< maximum uart baud rate */
-#define AT_UART_BAUD_RATE_MIN                       80                      /**< minimum uart baud rate */
+#define AT_UART_BAUD_RATE_MIN                       2400                    /**< minimum uart baud rate */
 #define AT_UART_BAUD_RATE_DEF                       115200                  /**< default uart baud rate */
 #define AT_UART_PATTERN_TIMEOUT_MS                  20                      /**< uart pattern timeout */
 
@@ -40,8 +40,8 @@
 #define AT_UART_QUEUE_SIZE                          15                      /**< uart queue size */
 #else
 #define AT_UART_RX_BUFFER_SIZE                      2048                    /**< uart rx buffer size */
-#define AT_UART_TX_BUFFER_SIZE                      8192                    /**< uart tx buffer size */
-#define AT_UART_QUEUE_SIZE                          30                      /**< uart queue size */
+#define AT_UART_TX_BUFFER_SIZE                      4096                    /**< uart tx buffer size */
+#define AT_UART_QUEUE_SIZE                          3000                    /**< uart queue size */
 #endif
 
 /**
